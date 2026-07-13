2af1b2b48e2b5604d694eba8a2468b8a9edc580a
Pushpalalitha3-B <bondalapushpa3@gmail.com>
2026-07-13 20:52:34 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# data-science-project
-My Python and data science learing projects
+# House Price Prediction
+
+## Project Description
+This project predicts the price of a house using Machine Learning.
+
+## Technologies Used
+- Python
+- Pandas
+- Matplotlib
+- Scikit-learn
+
+## Features
+- Predicts house prices
+- Uses Linear Regression
+- Displays a graph of house prices
--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-print("Hello, GitHub!")
-print("This is my first Python program.")
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+# Load dataset
+df = pd.read_csv("house_data.csv")
+
+# Features and target
+X = df[["Area", "Bedrooms"]]
+y = df["Price"]
+
+# Train model
+model = LinearRegression()
+model.fit(X, y)
+
+# User input
+area = int(input("Enter house area (sq.ft): "))
+bedrooms = int(input("Enter number of bedrooms: "))
+
+# Predict
+price = model.predict([[area, bedrooms]])
+
+print("\n========== RESULT ==========")
+print("Area:", area)
+print("Bedrooms:", bedrooms)
+print("Predicted House Price: ₹", round(price[0], 2))
+
+# Graph
+plt.scatter(df["Area"], df["Price"])
+plt.xlabel("Area (sq.ft)")
+plt.ylabel("Price (₹)")
+plt.title("House Price Prediction")
+plt.show()
